9531f6de29623d1055e0c1ebe4ec237b5f1955e8
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2022-04-26 19:19:58 -0700

    Add ShellSpec to the PATH

--- src/install_shellspec.sh
+++ src/install_shellspec.sh
@@ -7,11 +7,12 @@ set -euo pipefail
 IFS=$'\n\t'
 
 # Install latest ShellSpec
-
 curl \
     --fail \
     --location https://git.io/shellspec \
     --show-error \
     --silent \
     | sh -s -- --yes
-shellspec --init
+echo "export PATH='$HOME/.local/lib/shellspec:$PATH'" >> ~/.profile
+$HOME/.local/lib/shellspec/shellspec --init
+

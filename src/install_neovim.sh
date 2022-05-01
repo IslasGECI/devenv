876519565f800e308e0afce6df81739592c0caee
Evaristo Rojas • GECI <evaristo.rojas@islas.org.mx>
2022-04-30 20:11:14 -0700

    Update install_neovim.sh

--- src/install_neovim.sh
+++ src/install_neovim.sh
@@ -12,4 +12,4 @@ chmod u+x "$HOME"/nvim.appimage
 "$HOME"/nvim.appimage --appimage-extract
 
 # Install vim-plug
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim

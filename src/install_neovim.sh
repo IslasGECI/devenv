2fc98b7f50981e844a9404d7421227bce96b938e
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2022-04-30 19:02:38 -0700

    Install vim-plug

--- src/install_neovim.sh
+++ src/install_neovim.sh
@@ -10,3 +10,7 @@ cd "$HOME"
 wget --directory-prefix="$HOME" https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
 chmod u+x "$HOME"/nvim.appimage
 "$HOME"/nvim.appimage --appimage-extract
+
+# Install vim-plug
+curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+"$HOME/squashfs-root/usr/bin/nvim" -es -u "$HOME/.config/nvim/init.vim" -i NONE -c "PlugInstall" -c "qa"

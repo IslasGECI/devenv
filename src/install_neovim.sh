de3eb64142918fb2daa34683e535086bcb56b2e7
Evaristo Rojas • GECI <evaristo.rojas@islas.org.mx>
2022-05-13 00:08:26 -0700

    Update install_neovim.sh

--- src/install_neovim.sh
+++ src/install_neovim.sh
@@ -1,15 +1,10 @@
-#!/usr/bin/env bash
+#!/usr/bin/env bash -euo pipefail
 #
-# Install latest Neovim from GitHub
-
-# unofficial strict mode: http://redsymbol.net/articles/unofficial-bash-strict-mode/
-set -euo pipefail
-
 # Install latest Neovim from GitHub
 cd "$HOME"
 wget --directory-prefix="$HOME" https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
 chmod u+x "$HOME"/nvim.appimage
 "$HOME"/nvim.appimage --appimage-extract
 
-# Install vim-plug
-curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+# Install packer.nvim
+git clone --depth 1 https://github.com/wbthomason/packer.nvim "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/pack/packer/start/packer.nvim

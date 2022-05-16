31fa3cbf1cd05c76298cb2998ceeedf2f7e9ddd7
Evaristo Rojas • GECI <evaristo.rojas@islas.org.mx>
2022-05-16 16:43:31 -0700

    Update install_dotfiles.sh

--- src/install_dotfiles.sh
+++ src/install_dotfiles.sh
@@ -2,10 +2,10 @@
 #
 # Install dotfiles from GitHub
 
-# unofficial strict mode: http://redsymbol.net/articles/unofficial-bash-strict-mode/
+# [Set strict mode](http://redsymbol.net/articles/unofficial-bash-strict-mode)
 set -euo pipefail
 
 # Install dotfiles from GitHub
-git clone --bare git@github.com:devarops/dotfiles.git ${HOME}/dotfiles.git
-git --git-dir=${HOME}/dotfiles.git --work-tree=${HOME} checkout
-git --git-dir=${HOME}/dotfiles.git --work-tree=${HOME} config --local status.showUntrackedFiles no
+git clone --bare git@github.com:devarops/dotfiles.git "${HOME}/dotfiles.git"
+git --git-dir="${HOME}/dotfiles.git" --work-tree="${HOME}" checkout
+git --git-dir="${HOME}/dotfiles.git" --work-tree="${HOME}" config --local status.showUntrackedFiles no
